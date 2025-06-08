d4d11bac2f3fd9870491e03072e7e0541035a582
ahmedbna <hi@ahmedbna.com>
2025-06-08 04:10:29 +0300

    header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import { TailwindIndicator } from '@/components/tailwind-indicator';
 import { ThemeProvider } from '@/components/theme-provider';
 import { LayoutProvider } from '@/hooks/use-layout';
