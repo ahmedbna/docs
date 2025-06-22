755ae79c6e81278175453e34af91435f265317a2
ahmedbna <hi@ahmedbna.com>
2025-06-22 21:42:43 +0300

    test

---
+++ registry/examples/tooltip-demo.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/registry/ui/button';
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/registry/ui/tooltip';
+
+export default function TooltipDemo() {
+  return (
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <Button variant='outline'>Hover</Button>
+      </TooltipTrigger>
+      <TooltipContent>
+        <p>Add to library</p>
+      </TooltipContent>
+    </Tooltip>
+  );
+}
